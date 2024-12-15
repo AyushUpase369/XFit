e9b99ae17cb5869ae63253c920dff778b40fc757
ayush1 <ayushsunilupase999@gmail.com>
2024-12-15 23:38:48 +0530

    First Run

---
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.first
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val btn=findViewById<Button>(R.id.button)
+        val view=findViewById<TextView>(R.id.textView)
+        var v=0
+        btn.setOnClickListener {
+            v++
+            view.setText(""+v)
+        }
+    }
+
+}
