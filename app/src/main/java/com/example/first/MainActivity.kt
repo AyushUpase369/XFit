17b7afe699819c45288917671ffdbac344d50fcb
ayush1 <ayushsunilupase999@gmail.com>
2024-12-18 22:19:01 +0530

    add increase & decrease btn

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -15,10 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn=findViewById<Button>(R.id.button)
         val view=findViewById<TextView>(R.id.textView)
+        val btn2=findViewById<Button>(R.id.button2)
         var v=0
         btn.setOnClickListener {
             v++
-            view.setText("You Clicked Button "+v+" times")
+            view.setText(""+v)
+        }
+        btn2.setOnClickListener{
+            v--
+            view.setText(""+v)
         }
     }
 
